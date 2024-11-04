5b4a49ff2c706dd55a90a220ec678da42eb0a77e
pan-bubr <19240387+adrian-golawski@users.noreply.github.com>
2024-11-04 11:16:03 +0100

    Initial setup

---
+++ main.ts
@@ -0,0 +1,35 @@
+// Read input from CLI Args
+let day = Deno.args[0];
+let inputFilePath = Deno.args[1];
+
+// If there is no input, ask for the std in
+if (!day) day = prompt("What day do you want to solve?: ") || "";
+if (!inputFilePath) {
+  inputFilePath = prompt("Pass me the path to the input file: ") || "";
+}
+
+// Read input string
+const input = (await Deno.readTextFile(inputFilePath).catch(() => {
+  alert(`Error: Not able to read the file: ${inputFilePath}`);
+  Deno.exit(1);
+})).trim();
+
+// Solve the question
+import day1 from "./solutions/day1.ts";
+
+let part1: string = "";
+let part2: string = "";
+
+switch (day) {
+  case "1":
+    [part1, part2] = day1(input);
+    break;
+  default:
+    alert(`Day ${day} not implemented yet.`);
+}
+
+console.log(
+  `[Solution] 
+  Part 1: ${part1}
+  Part 2: ${part2}`,
+);
